e2b24bc7b379b1f16dec48088e249bf7fbf037c3
Dmitry Kostenko <codeforsmile@gmail.com>
2020-08-26 19:35:43 +0000

    Expanded console test
    Changed to 25K I/O scenario
    Added online logging for reads, timeouts and latency.

--- toys/TestConsole/Program.cs
+++ toys/TestConsole/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using StackExchange.Redis;
 
@@ -7,36 +10,82 @@ namespace TestConsole
 {
     internal static class Program
     {
-        public static async Task Main()
+        public static async Task Main(string[] args)
         {
-            var client = ConnectionMultiplexer.Connect("localhost");
-            client.GetDatabase().Ping();
-            var db = client.GetDatabase(0);
+            int N = 1000000;
+            int T = 10000;
+            ThreadPool.SetMinThreads(100, 100);
 
+            string connectionString = args.Length > 0 ? args[0] : "localhost";
+
+            ConcurrentQueue<ConnectionMultiplexer> clients = new ConcurrentQueue<ConnectionMultiplexer>();
+            for (int i = 0; i < 2; i++)
+            {
+                var client = ConnectionMultiplexer.Connect(connectionString);
+                client.GetDatabase().Ping();
+                clients.Enqueue(client);
+            }
+
+            bool terminated = false;
             var start = DateTime.Now;
 
-            Show(client.GetCounters());
+            clients.TryDequeue(out var firstClient);
+            clients.Enqueue(firstClient);
+            await firstClient.GetDatabase().StringSetAsync("A", new string('a', 25000));
 
-            var tasks = Enumerable.Range(0, 1000).Select(async i =>
+            int timeouts = 0;
+            int reads = 0;
+            long latency = 0;
+            Exception ex = null;
+
+            var tasks = Enumerable.Range(0, T).Select(async i =>
             {
                 int timeoutCount = 0;
                 RedisKey key = i.ToString();
-                for (int t = 0; t < 1000; t++)
+                for (int t = 0; t < N/T && !terminated; t++)
                 {
                     try
                     {
-                        await db.StringIncrementAsync(key, 1);
+                        //await db.StringIncrementAsync(key, 1);
+                        ConnectionMultiplexer client;
+                        while (!clients.TryDequeue(out client)) await Task.Delay(0);
+                        clients.Enqueue(client);
+
+                        var s = new Stopwatch();
+                        s.Start();
+
+                        await client.GetDatabase().StringGetAsync("A");
+
+                        Interlocked.Add(ref latency, (long)s.Elapsed.TotalMilliseconds);
+                        Interlocked.Increment(ref reads);
                     }
-                    catch (TimeoutException) { timeoutCount++; }
+                    catch (TimeoutException e)
+                    { 
+                        ex = e;
+                        // Console.WriteLine("Timeout. Adding a multiplexer");
+                        // clients.Enqueue(ConnectionMultiplexer.Connect(connectionString));
+                        Interlocked.Increment(ref timeouts);
+                    }
                 }
                 return timeoutCount;
             }).ToArray();
 
-            await Task.WhenAll(tasks);
-            int totalTimeouts = tasks.Sum(x => x.Result);
-            Console.WriteLine("Total timeouts: " + totalTimeouts);
+            var allTasks = Task.WhenAll(tasks);
+            while (!allTasks.Wait(0))
+            {
+                await Task.Delay(1000);
+                if (ex != null)
+                {
+                    Console.WriteLine(ex);
+                    ex = null;
+                }
+                Console.WriteLine($"R = {reads}, T = {timeouts}, L = {(double)latency/reads}");
+            }
+
+            // int totalTimeouts = tasks.Sum(x => x.Result);
+            Console.WriteLine("Total timeouts: " + timeouts);
             Console.WriteLine();
-            Show(client.GetCounters());
+            // Show(client.GetCounters());
 
             var duration = DateTime.Now.Subtract(start).TotalMilliseconds;
             Console.WriteLine($"{duration}ms");
